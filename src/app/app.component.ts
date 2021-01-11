0d39a36f5ab452f9c3ad6942e24eed8b9e73174f
hronika3 <sk_andr977@mail.ru>
2021-01-11 19:08:25 +0300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core'
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms'
+import {MyValidators} from './my.validators'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'validation';
+export class AppComponent implements OnInit {
+  form: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+        MyValidators.restrictedEmails
+      ], [MyValidators.uniqEmail]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    })
+  }
+
+  submit() {
+    if (this.form.valid) {
+      console.log('Form: ', this.form)
+      const formData = {...this.form.value}
+
+      console.log('Form Data:', formData)
+    }
+  }
 }
